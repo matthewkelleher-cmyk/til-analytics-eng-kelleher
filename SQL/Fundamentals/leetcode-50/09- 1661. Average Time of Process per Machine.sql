67c2969af3546e010dc513ca56b061d2dbbe50fd
Matthew Kelleher <matthew.kelleher@theinformationlab.com>
2026-09-15 10:11:36 -0400

    Add challenges 09 and 10

---
+++ SQL/Fundamentals/leetcode-50/09- 1661. Average Time of Process per Machine.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+with sub_table as (select 
+    machine_id, 
+    process_id, 
+    max(case when activity_type = 'start' then timestamp end) as start_time,
+    max(case when activity_type = 'end' then timestamp end) as end_time
+from activity
+group by machine_id, process_id)
+
+select 
+    machine_id,
+    round(avg(end_time - start_time), 3) as processing_time
+from sub_table
+group by machine_id
+;
